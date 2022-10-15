c2179ae0d17d6e2abdef2e64575f75b269b52b17
eballhysa <ballhysa@hotmail.com>
2022-10-15 23:37:47 +0200

    new and create actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,20 @@ class MoviesController < ApplicationController
     id = params[:id]
     @movie = Movie.find id
   end
+
+  def new
+    @movie = Movie.new
+    # default: render 'new'
+  end
+
+  def create
+    Movie.create! movie_params
+    redirect_to movies_path
+  end
+
+  private
+  def movie_params
+    params.require(:movie)
+          .permit(:title, :rating, :release_date)
+  end
 end
