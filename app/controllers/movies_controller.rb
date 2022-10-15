4f36ec449a85fd7aeb4248ce86ff7611f8555364
eballhysa <ballhysa@hotmail.com>
2022-10-15 23:56:10 +0200

    flash & redirection - fix

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,9 +14,20 @@ class MoviesController < ApplicationController
   end
 
   def create
-    Movie.create! movie_params
+    @movie = Movie.create! movie_params
     flash[:notice] = "Movie #{@movie.title} was successfully created!"
     redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update! movie_params
+    flash[:notice] = "Movie #{@movie.title} successfully updated!"
+    redirect_to movie_path(@movie)
   end
 
   private
